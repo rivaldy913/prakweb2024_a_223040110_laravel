dd0e1aa62cf9da11de7e799314e32c17340d0a08
rivaldy913 <rivaldynovyandp@gmail.com>
2024-11-22 06:52:46 +0700

    Video 6 View Data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,8 +11,60 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Rivaldy Novyan Dwi Putra', 'title' => 'Contact']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Rivaldy Novyan Dwi Putra',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Cupiditate nam accusantium esse neque repudiandae totam, 
+            at officiis praesentium quas, officia quidem eligendi facilis 
+            sapiente ex voluptas, minus corrupti. Eos, earum.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Rivaldy Novyan Dwi Putra',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Cupiditate nam accusantium esse neque repudiandae totam, 
+            at officiis praesentium quas, officia quidem eligendi facilis 
+            sapiente ex voluptas, minus corrupti. Eos, earum!'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Rivaldy Novyan Dwi Putra',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Cupiditate nam accusantium esse neque repudiandae totam, 
+            at officiis praesentium quas, officia quidem eligendi facilis 
+            sapiente ex voluptas, minus corrupti. Eos, earum.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Rivaldy Novyan Dwi Putra',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Cupiditate nam accusantium esse neque repudiandae totam, 
+            at officiis praesentium quas, officia quidem eligendi facilis 
+            sapiente ex voluptas, minus corrupti. Eos, earum!'
+        ]
+     ];
+
+        $post = Arr::first($posts, function ($post) use ($slug){
+            return $post['slug'] ==  $slug;
+        });
+
+        return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
