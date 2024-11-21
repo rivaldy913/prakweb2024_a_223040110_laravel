b152a88da9b0eed8511f9d614890d56e0a1af3a4
rivaldy913 <rivaldynovyandp@gmail.com>
2024-11-21 08:43:49 +0700

    Video 4 Blade Templating Engine

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,6 +10,8 @@
 <body>
     <a href="/">Home</a>
     <a href="/about">About</a>
+    <a href="/blog">Blog</a>
+    <a href="/contact">Contact</a>
     <h1>Halaman About Rivaldy</h1>
     <h3>Owner: {{ $nama }}</h3>
     <img src="img/rivaldy.jpg" alt="Rivaldy Novyan Dwi Putra" width="200">
--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -119,13 +119,13 @@
   
     <header class="bg-white shadow">
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home Page</h1>
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Blog</h1>
       </div>
     </header>
     <main>
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <!-- Your content -->
-        <p>Welcome to my home page!</p>
+        <p>Welcome to my blog!</p>
       </div>
     </main>
   </div>
