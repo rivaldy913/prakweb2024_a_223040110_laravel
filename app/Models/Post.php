744f59391b8c6eaeab3aab847bccbdce0afb19d4
rivaldy913 <rivaldynovyandp@gmail.com>
2024-11-22 14:33:23 +0700

    Video 9 Eloquent ORM & Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,47 +1,12 @@
-<?php  
+<?php
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Rivaldy Novyan Dwi Putra',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Cupiditate nam accusantium esse neque repudiandae totam, 
-                at officiis praesentium quas, officia quidem eligendi facilis 
-                sapiente ex voluptas, minus corrupti. Eos, earum.'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Rivaldy Novyan Dwi Putra',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Cupiditate nam accusantium esse neque repudiandae totam, 
-                at officiis praesentium quas, officia quidem eligendi facilis 
-                sapiente ex voluptas, minus corrupti. Eos, earum!'
-            ]
-            ];
-    }
-
-    public static function find($slug): array
-
-    {
-
-        $post = Arr::first(static::all(), fn($post) => $post['slug'] == $slug);
-        
-        if(! $post) {
-            abort(404);
-        }
-
-        return $post;
-    }
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
