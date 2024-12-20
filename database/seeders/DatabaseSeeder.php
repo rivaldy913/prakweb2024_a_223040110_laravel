a4fdb0d67d14af6d205f82942675889dfcc29136
rivaldy913 <rivaldynovyandp@gmail.com>
2024-12-20 08:52:34 +0700

    Video 13 Database Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,14 @@
 
 namespace Database\Seeders;
 
+use App;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +20,29 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+       // User::factory()->create([
+         //   'name' => 'Test User',
+           // 'email' => 'test@example.com',
+         // ]);
+
+    //   Category::create([
+     //     'name' => 'Web Design',
+       //   'slug' => 'web-design'
+     //  ]);
+
+      // Post::create([
+        //  'title' => 'Judul Artikel 1',
+        //  'author_id' => 1,
+        //  'category_id' => 1,
+        //  'slug' => 'judul-artikel-1',
+        //  'body' =>' Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti ullam nihil culpa explicabo asperiores ipsam, nulla quaerat, voluptas assumenda voluptates nemo quisquam exercitationem illo natus fuga. Ratione corrupti quia unde?'
+       //]);
+
+       $this->call([CategorySeeder::class, UserSeeder::class]);
+       Post::factory(100)->recycle([
+            Category::all(),
+            User::all()
+       ])->create();
+   
     }
 }
